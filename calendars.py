6d21c26d4f98b3016a793cc5daee0a4751469579
Kevin Joyner <joyner.kevin@gmail.com>
2017-05-03 22:00:22 +0100

    attempt to optimise code

--- calendars.py
+++ calendars.py
@@ -21,10 +21,9 @@ from oauth2client import tools
 from oauth2client.file import Storage
 
 # Import your private configuration variables.
-
 from config import *
 
-# These are
+# which are
 # - CLIENT_SECRET_FILE
 # (Set this to the file name of your client secret JSON file.)
 # - WORK_EMAIL
@@ -33,6 +32,7 @@ from config import *
 # - WORK_PERSONAL_CAL_ID
 # (Create secondary calendars in your personal Google account, one for work and one for
 # personal events. These are the IDs for each.)
+# - MY_DISPLAY_NAME
 
 # Sets things needed to authenticate with Google API
 FLAGS = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
@@ -45,6 +45,7 @@ APPLICATION_NAME = 'API thing'
 def get_credentials(flags):
     """ A function for retrieving and storing Google credentials. It returns a Google APIs
      credentials object. """
+    
     credential_path = CREDENTIALS_DIR + 'google-credentials.json'
     store = Storage(credential_path)
     credentials = store.get()
@@ -56,68 +57,81 @@ def get_credentials(flags):
         print('Storing credentials to ' + credential_path)
     return credentials
 
-def main():
-    """ The main function of the script """
-    credentials = get_credentials(FLAGS)
-    http = credentials.authorize(httplib2.Http())
-    service = discovery.build('calendar', 'v3', http=http)
+def get_events_list(service, cal_id):
+    """ Fetches list of events from specified calendar ID. """
 
     # Prepares min and max dates for the oldest and newest events to handle
     months_ago = (datetime.datetime.utcnow() - timedelta(days=93)).isoformat() + 'Z'
     months_away = (datetime.datetime.utcnow() + timedelta(days=93)).isoformat() + 'Z'
 
-    # Fills a biz_events list with the events from the business calendar
-    biz_events = []
+    output = []
     page_token = None
     while True:
         events_result = service.events().list(
-            calendarId=WORK_EMAIL, pageToken=page_token,
+            calendarId=cal_id, pageToken=page_token,
             singleEvents=True, orderBy='startTime',
             showDeleted=True, showHiddenInvitations=True,
             timeMin=months_ago, timeMax=months_away
         ).execute()
-        biz_events.extend(events_result.get('items'))
+        output.extend(events_result.get('items'))
         page_token = events_result.get('nextPageToken')
         if not page_token:
             break
 
-    # Fills a biz_import list with the work events modified for import to personal account
-    biz_import = []
-    for event in biz_events:
-        declined = False
-        if 'attendees' in event: # not all events have attendees
-            for attendee in event['attendees']:
-                # finds you (at work) in the list of attendees
-                if attendee['email'] == WORK_EMAIL:
-                    if attendee['responseStatus'] == 'declined':
-                        # Having discovered that you're not planning to attend, saves
-                        # this for later.
-                        declined = True
-        new_event = event
+    return output
+
+def decline_check(event, declining_email):
+    """ Returns boolean indicating whether an event has been declined by the
+     specified email address."""
+
+    declined = False
+    if 'attendees' in event: # not all events have attendees
+        for attendee in event['attendees']:
+            # finds you in the list of attendees
+            if attendee['email'] == declining_email:
+                if attendee['responseStatus'] == 'declined':
+                    # Having discovered that you're not planning to attend, saves
+                    # this for later.
+                    declined = True
+
+    return declined
+
+def prep_import_from_work(events_list):
+    """ Prepares work events for import to personal calendar. """
+
+    output = []
+    for event in events_list:
+        declined = decline_check(event, WORK_EMAIL)
 
         # Tags the events in the description field with something distinctive
-        new_event['description'] = event.get('description', '') + \
-                                                        '\n\n## sync\'d from work ##'
+        event['description'] = event.get('description', '') + \
+                                                        '## sync\'d from work ##'
 
         # Strips off attendees so that you don't accidentally re-invite all your client
         # contacts to meetings that have been cancelled, or execute some similarly
         # career-limiting mistake.
-        new_event.pop('attendees', None)
+        event.pop('attendees', None)
 
         # Fixes your personal self as the organizer, so that the events always remain
         # compatible with the fickle laws of calendars: i.e. you do need to be
         # either attending or organising a meeting that's in your calendar.
-        new_event['organizer'] = {
-            'displayName': 'Kevin Joyner',
+        event['organizer'] = {
+            'displayName': MY_DISPLAY_NAME,
             'email': PERSONAL_EMAIL,
             'self': True
             }
         # Converts declined attendence into having cancelled your own appointment.
         if declined is True:
-            new_event['status'] = 'cancelled'
-        biz_import.append(new_event)
+            event['status'] = 'cancelled'
+        output.append(event)
 
-    for event in biz_import:
+    return output
+
+def import_events(service, events_list, dest_cal_id, orig_cal_id):
+    """ Imports list of events to the destination calendar. Uses the originating calendar to work
+     around sequence number errors if they occur. """
+
+    for event in events_list:
         # Removes the immutable IDs from the event, but saves id in case it's needed.
         # I think it's the standard iCalUID, which remains on the events, that means
         # this script can run every hour without causing loads of duplicate events.
@@ -134,57 +148,36 @@ def main():
                 sleep(wait / 10) # waits 0.225 seconds
                 # then imports the event to the primary personal calendar (which your work Google
                 # account needs to be able to access)
-                service.events().import_(calendarId=PERSONAL_EMAIL, body=event).execute()
+                service.events().import_(calendarId=dest_cal_id, body=event).execute()
             except HttpError as err:
                 if err.resp.status in [403, 500, 503]:
                     wait = wait * wait # If e.g. a Rate Limit Exceeded error, squares the wait time
                 elif 'Invalid sequence value.' in err.resp.get('message', ''):
                     event_lookup = service.events().get(
-                        calendarId=WORK_EMAIL, eventId=originating_id
+                        calendarId=orig_cal_id, eventId=originating_id
                     ).execute()
                     event['sequence'] = event_lookup.get('sequence', 99)
                 else: raise
             else: break
 
-    # Fills an all_events list with a complete list of eveything taken back from the
-    # personal account.
-    all_events = []
-    page_token = None
-    while True:
-        events_result = service.events().list(
-            calendarId=PERSONAL_EMAIL, pageToken=page_token,
-            singleEvents=True, orderBy='startTime',
-            showDeleted=True, showHiddenInvitations=True,
-            timeMin=months_ago, timeMax=months_away
-        ).execute()
-        all_events.extend(events_result.get('items'))
-        page_token = events_result.get('nextPageToken')
-        if not page_token:
-            break
+def divide_all_events(all_events):
+    """ Divides the list of all events take back out of the personal calendar into separat
+     personal and work lists. """
 
-
-    # Fills work_ and personal_events lists with the personal account versions of recent events
-    work_events = []
-    personal_events = []
+    work_events = personal_events = []
     for event in all_events:
-        new_event = event
-        # Preps the events for import in the same way as before.
-        # (I'm sure some of this could be factored into a separate function.)
-        declined = False
-        if 'attendees' in new_event: # not all events have attendees
-            for attendee in new_event['attendees']:
-                # finds you in the list of attendees
-                if attendee['email'] == PERSONAL_EMAIL:
-                    if attendee['responseStatus'] == 'declined':
-                        # Having discovered that you're not planning to attend, saves
-                        # this for later.
-                        declined = True
-        new_event.pop('attendees', None)
+        declined = decline_check(event, PERSONAL_EMAIL)
+
+        # Strips off attendees so that you don't accidentally re-invite all your client
+        # contacts to meetings that have been cancelled, or execute some similarly
+        # career-limiting mistake.
+        event.pop('attendees', None)
+
         # Fills the two lists based on the presence of the description field work tag.
-        if 'description' in new_event:
-            if '## sync\'d from work ##' in new_event['description']:
+        if 'description' in event:
+            if '## sync\'d from work ##' in event['description']:
                 # Sets the right organiser for the calendar the event is going into.
-                new_event['organizer'] = {
+                event['organizer'] = {
                     'displayName': 'Kevin - Work only',
                     'email': WORK_PERSONAL_CAL_ID,
                     'self': True
@@ -193,76 +186,64 @@ def main():
                 # for someone getting into my home and playing a recording of my voice to my
                 # Google Home, I'm in-line with my employer's permissions - by forcing
                 # 'private' visibility.
-                new_event['visibility']	= 'private'
-                work_events.append(new_event)
+                event['visibility']	= 'private'
+                work_events.append(event)
             else:
                 # Sets the right organiser for the calendar the event is going into.
-                new_event['organizer'] = {
+                event['organizer'] = {
                     'displayName': 'Kevin - Personal only',
                     'email': PERSONAL_PERSONAL_CAL_ID,
                     'self': True
                 }
                 # Converts declined attendence into having cancelled your own appointment.
                 if declined is True:
-                    new_event['status'] = 'cancelled'
-                personal_events.append(new_event)
+                    event['status'] = 'cancelled'
+                personal_events.append(event)
         else:
             # I don't know if this happens but if there was no description at all, it couldn't
             # be a work event.
-            new_event['organizer'] = {
+            event['organizer'] = {
                 'displayName': 'Kevin - Personal only',
                 'email': PERSONAL_PERSONAL_CAL_ID,
                 'self': True
             }
             if declined is True:
-                new_event['status'] = 'cancelled'
-            personal_events.append(new_event)
+                event['status'] = 'cancelled'
+            personal_events.append(event)
+    return {'work_events': work_events, 'personal_events': personal_events}
+
+def main():
+    """ The main function of the script """
+    credentials = get_credentials(FLAGS)
+    http = credentials.authorize(httplib2.Http())
+    service = discovery.build('calendar', 'v3', http=http)
+
+    # Fills a biz_events list with the events from the business calendar
+    biz_events = get_events_list(service, WORK_EMAIL)
+
+    # Fills a biz_import list with the work events modified for import to personal account
+    biz_import = prep_import_from_work(biz_events)
+
+    import_events(service, biz_import, PERSONAL_EMAIL, WORK_EMAIL)
+
+    # Fills an all_events list with a complete list of eveything taken back from the
+    # personal account.
+    all_events = get_events_list(service, PERSONAL_EMAIL)
+
+    # Fills work_ and personal_events lists with the personal account versions of recent events
+    divided_lists = divide_all_events(all_events)
 
     # Imports the personal events into the personal secondary calendar in my personal account
-    for event in personal_events:
-        originating_id = event.pop('id', None)
-        event.pop('recurringEventId', None)
-        wait = 2.25
-        while True:
-            try:
-                sleep(wait / 10)
-                service.events().import_(
-                    calendarId=PERSONAL_PERSONAL_CAL_ID,
-                    body=event
-                ).execute()
-            except HttpError as err:
-                if err.resp.status in [403, 500, 503]:
-                    wait = wait * wait
-                elif 'Invalid sequence value.' in err.resp.get('message', ''):
-                    event_lookup = service.events().get(
-                        calendarId=PERSONAL_PERSONAL_CAL_ID, eventId=originating_id
-                    ).execute()
-                    event['sequence'] = event_lookup.get('sequence', 99)
-                else: raise
-            else: break
+    import_events(
+        service, divided_lists.get('personal_events', None),
+        PERSONAL_PERSONAL_CAL_ID, PERSONAL_EMAIL
+    )
 
     # Imports the work events into the work secondary calendar in my personal account
-    for event in work_events:
-        originating_id = event.pop('id', None)
-        event.pop('recurringEventId', None)
-        wait = 2.25
-        while True:
-            try:
-                sleep(wait / 10)
-                service.events().import_(
-                    calendarId=WORK_PERSONAL_CAL_ID,
-                    body=event
-                ).execute()
-            except HttpError as err:
-                if err.resp.status in [403, 500, 503]:
-                    wait = wait * wait
-                elif 'Invalid sequence value.' in err.resp.get('message', ''):
-                    event_lookup = service.events().get(
-                        calendarId=WORK_PERSONAL_CAL_ID, eventId=originating_id
-                    ).execute()
-                    event['sequence'] = event_lookup.get('sequence', 99)
-                else: raise
-            else: break
+    import_events(
+        service, divided_lists.get('work_events', None),
+        WORK_PERSONAL_CAL_ID, PERSONAL_EMAIL
+    )
 
 if __name__ == '__main__':
     main()
