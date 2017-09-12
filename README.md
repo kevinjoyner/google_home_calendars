4f71ac0d4ea49e97f94a75eeee34b56154f2fbf1
Kevin Joyner <joyner.kevin@gmail.com>
2017-09-12 19:31:24 +0100

    try syncToken approach remove del_cancels() which should be unnecessary tidy up a bit

--- README.md
+++ README.md
@@ -2,10 +2,10 @@
 Python script for organising work and personal calendars to make Google Home better
 
 ## The problem
-Your most important calendar is your work one, but Google Home needs to be connected to your peronsal Google account. Google is adding support for multiple linked accounts, but it won't handle multiple accounts for the same voice (i.e. for the same person).
+Your most important calendar is your work one, but Google Home needs to be connected to your peronsal Google account. Google has added support for multiple linked accounts, but it doesn't handle multiple accounts for the same voice (i.e. for the same person).
 
 ## The solution
-You can give your work Google account full permission over your personal calendars. This script uses your work credentials to copy - safely! - your work events into your primary personal calendar, and then also into two separate secondary calendars in your personal account.
+You can give your work Google user account full permission over your personal calendars. This script uses your work credentials to copy - safely! - your work events into your primary personal calendar, and then also to copy your work and personal events into two separate secondary calendars in your personal account.
 
 You keep the separation you need for yourself, but the Google Assistant can now see appointments for everything you're doing, and be much more helpful.
 
@@ -13,18 +13,18 @@ I run the script hourly with cron, from a linux server.
 
 ## Feature details
 The script
-- Works with events as old as six months ago, and up to six months into the future
+- Configurable to work with events within a specific relative date range, e.g. as old as six months ago, and up to six months into the future
 - Tags your work events as they're copied into your personal account, by adding a line to the description, "## sync'd from work ##"
 - Removes all attendees from the copies of your events...
-- ... and fixes you as the organiser of copied events. Removing attendees removes the risk of accidentally inviting people to duplicate events; and it keeps work contact email addresses from being copied anywhere.
-- Transfers cancellations to keep your calendars aligned
+- ... and so also fixes you (at your personal email address) as the organiser of copied events. Removing attendees removes the risk of accidentally inviting people to duplicate events; and it keeps any of your contacts' email addresses from being copied anywhere.
+- Transfers changed and cancelled events, to keep your calendars sync'd
 - Converts having declined to attend an origin calendar event into a cancellation in the destination (where you are the organizer of events that have no attendees)
 - In the work secondary calendar in your personal Google account, the events are all set to Private visibility, ensuring restricted access to work information.
 
 ## Apologies
 - There are bits of the code that I'm sure are inelegant and inefficient.
-- I've used OAuth for authentication with Google. I run the script locally and then allow the credentials token to be pushed to my server. This works for me, but no doubt it's stupid and bad pratice or something: I think there's a better way for a server application to authenticate with the Google APIs.
+- I've used OAuth for authentication with Google. I run the script locally and then allow the credentials token to be pushed to my server. This works okay, but I need to get round to swapping this for service account credentials, which are better suited to a script running from a server.
 
 ## Authentication and privacy
 - The .gitignore file on Github excludes files in the credentials folder. If you use git to deploy to a server that the script runs from, you can move your credentials files to the server manually.
-- I wanted to use one repo for my server and for Github, so I've moved the personal configuration variables (email addresses and calendar IDs) into a separate file that gets imported by the main script. I'm hiding this file, "/config.py", from the repo with the .gitignore file.
+- To avoid them being posted on Github, configuration variables (including email addresses and calendar IDs) are in a separate JSON file that's accessed by the main script. This file, "config.json", is also excluded from the repo with the .gitignore file.
--- calendars.py
+++ calendars.py
@@ -9,12 +9,13 @@ secondary calendar for just the work events, and one for the personal ones.
 
 """
 
+import json
 import datetime
 from datetime import timedelta
 from time import sleep
 import argparse
-import dateutil.parser
-import pytz
+# import dateutil.parser
+# import pytz
 import httplib2
 from apiclient import discovery
 from apiclient.errors import HttpError
@@ -22,19 +23,19 @@ from oauth2client import client
 from oauth2client import tools
 from oauth2client.file import Storage
 
-# Import your private configuration variables.
-from config import *
 
-# which are
-# - CLIENT_SECRET_FILE
-# (Set this to the file name of your client secret JSON file.)
-# - WORK_EMAIL
-# - PERSONAL_EMAIL
-# - PERSONAL_PERSONAL_CAL_ID
-# - WORK_PERSONAL_CAL_ID
-# (Create secondary calendars in your personal Google account, one for work and one for
-# personal events. These are the IDs for each.)
-# - MY_DISPLAY_NAME
+# Load your private configuration variables.
+with open('config.json', 'r') as config_file:
+    CONFIG_JSON = json.load(config_file)
+    CREDENTIALS_DIR = CONFIG_JSON[1]["CREDENTIALS_DIR"]
+    CLIENT_SECRET_PATH = CONFIG_JSON[1]["CLIENT_SECRET_PATH"]
+    WORK_EMAIL = CONFIG_JSON[1]["WORK_EMAIL"]
+    PERSONAL_EMAIL = CONFIG_JSON[1]["PERSONAL_EMAIL"]
+    WORK_SYNC_TOKEN = CONFIG_JSON[1]["WORK_SYNC_TOKEN"]
+    PERSONAL_SYNC_TOKEN = CONFIG_JSON[1]["PERSONAL_SYNC_TOKEN"]
+    PERSONAL_PERSONAL_CAL_ID = CONFIG_JSON[1]["PERSONAL_PERSONAL_CAL_ID"]
+    WORK_PERSONAL_CAL_ID = CONFIG_JSON[1]["WORK_PERSONAL_CAL_ID"]
+    MY_DISPLAY_NAME = CONFIG_JSON[1]["MY_DISPLAY_NAME"]
 
 # Sets things needed to authenticate with Google API
 FLAGS = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
@@ -51,41 +52,68 @@ MONTHS_AWAY = (datetime.datetime.utcnow() + timedelta(days=93)).isoformat() + 'Z
 
 def get_credentials(flags):
     """ A function for retrieving and storing Google credentials. It returns a Google APIs
-     credentials object. """
-
+        credentials object. """
     credential_path = CREDENTIALS_DIR + 'google-credentials.json'
     store = Storage(credential_path)
     credentials = store.get()
     if not credentials or credentials.invalid:
-        flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
+        flow = client.flow_from_clientsecrets(CLIENT_SECRET_PATH, SCOPES)
         flow.user_agent = APPLICATION_NAME
         flags = tools.argparser.parse_args(args=[])
         credentials = tools.run_flow(flow, store, flags)
         print('Storing credentials to ' + credential_path)
     return credentials
 
-def get_events_list(service, cal_id):
+def get_events_list(service, cal_id, sync_token):
     """ Fetches list of events from specified calendar ID. """
-
     output = []
     page_token = None
     while True:
-        events_result = service.events().list(
-            calendarId=cal_id, pageToken=page_token,
-            singleEvents=True, orderBy='startTime',
-            showDeleted=True, showHiddenInvitations=True,
-            timeMin=MONTHS_AGO, timeMax=MONTHS_AWAY
-        ).execute()
+        if sync_token:
+            try:
+                events_result = service.events().list(
+                    calendarId=cal_id,
+                    pageToken=page_token,
+                    syncToken=sync_token,
+                    timeMin=MONTHS_AGO, timeMax=MONTHS_AWAY
+                ).execute()
+            except HttpError as err:
+                if err.resp.status in [401]:
+                    if cal_id == WORK_EMAIL:
+                        CONFIG_JSON[1]["WORK_SYNC_TOKEN"] = sync_token = None
+                    elif cal_id == PERSONAL_EMAIL:
+                        CONFIG_JSON[1]["PERSONAL_SYNC_TOKEN"] = sync_token = None
+                    with open('config.json', 'w') as config_out_file:
+                        json.dump(CONFIG_JSON, config_out_file)
+                    events_result = service.events().list(
+                        calendarId=cal_id, pageToken=page_token,
+                        singleEvents=True, orderBy='startTime',
+                        showDeleted=True, showHiddenInvitations=True,
+                        timeMin=MONTHS_AGO, timeMax=MONTHS_AWAY
+                    ).execute()
+                else: raise
+        else:
+            events_result = service.events().list(
+                calendarId=cal_id, pageToken=page_token,
+                singleEvents=True, orderBy='startTime',
+                showDeleted=True, showHiddenInvitations=True,
+                timeMin=MONTHS_AGO, timeMax=MONTHS_AWAY
+            ).execute()
         output.extend(events_result.get('items'))
         page_token = events_result.get('nextPageToken')
         if not page_token:
+            if cal_id == WORK_EMAIL:
+                CONFIG_JSON[1]["WORK_SYNC_TOKEN"] = events_result.get('nextSyncToken')
+            elif cal_id == PERSONAL_EMAIL:
+                CONFIG_JSON[1]["PERSONAL_SYNC_TOKEN"] = events_result.get('nextSyncToken')
+            with open('config.json', 'w') as config_out_file:
+                json.dump(CONFIG_JSON, config_out_file)
             break
     return output
 
 def decline_check(event, declining_email):
     """ Returns boolean indicating whether an event has been declined by the
-     specified email address. """
-
+        specified email address. """
     declined = False
     if 'attendees' in event: # not all events have attendees
         for attendee in event['attendees']:
@@ -100,32 +128,23 @@ def decline_check(event, declining_email):
 
 def prep_import_from_work(events_list):
     """ Prepares work events for import to personal calendar. """
-
     output = []
     for event in events_list:
         declined = decline_check(event, WORK_EMAIL)
-
         # If personal email is organiser or attendee, then skip this event
         if event['organizer']['email'] == PERSONAL_EMAIL:
             continue
-
-        personally_invited = False
         if 'attendees' in event:
             for attendee in event['attendees']:
                 if attendee['email'] == PERSONAL_EMAIL:
-                    personally_invited = True
-        if personally_invited is True:
-            continue
-
+                    continue
         # Tags the events in the description field with something distinctive
         event['description'] = event.get('description', '') + \
                                                         '\\n\\n## sync\'d from work ##'
-
         # Strips off attendees so that you don't accidentally re-invite all your client
         # contacts to meetings that have been cancelled, or execute some similarly
         # career-limiting mistake.
         event.pop('attendees', None)
-
         # Fixes your personal self as the organizer, because a calendar requires
         # that you are either attending or organising a meeting that's in your calendar.
         event['organizer'] = {
@@ -133,19 +152,15 @@ def prep_import_from_work(events_list):
             'email': PERSONAL_EMAIL,
             'self': True
             }
-
         # Converts declined attendence into having cancelled your own appointment.
         if declined is True:
             event['status'] = 'cancelled'
         output.append(event)
-
     return output
-
 
 def request_backoff(service, cal_id, request_type, request_data):
     """ Wraps either a delete or import request in an "exponential backoff,"
-     I think... """
-
+        I think... """
     # The Google Calendar API can take 5 requests per second, apparently. I don't know
     # how you're really supposed to implement "exponential backoff" but I think I need
     # this 'wait' value number - the seconds to wait between requests - to be greater
@@ -165,124 +180,36 @@ def request_backoff(service, cal_id, request_type, request_data):
             else: raise
         else: break
 
-
-def del_cancels(service, events_list, cal_id):
-    """ Deletes from the specified calendar, based on the iCalUID, all events in the list that
-     are cancelled. Returns the events list with the deleted events removed. """
-
-    for_deletion = []
-    for event in events_list:
-        if event.get('status', None) == 'cancelled':
-            # The prep_import_from_work function has already 'converted' declines
-            # into cancellations.
-            for_deletion.append(event)
-            events_list.remove(event)
-
-    for event in for_deletion:
-        matching_events = []
-        page_token = None
-        while True:
-            # On this list request, no need to show deleted events
-            events_result = service.events().list(
-                calendarId=cal_id, pageToken=page_token,
-                singleEvents=True, orderBy='startTime',
-                showDeleted=False, showHiddenInvitations=True,
-                timeMin=MONTHS_AGO, timeMax=MONTHS_AWAY,
-                iCalUID=event.get('iCalUID', '')
-            ).execute()
-            matching_events.extend(events_result.get('items'))
-            page_token = events_result.get('nextPageToken')
-            if not page_token:
-                break
-
-        # Builds a list of event IDs to delete, so as to be able to avoid duplication
-        ids_to_delete = []
-        # for item in matching_events:
-        #     # Avoids deleting whole series of recurring events by building IDs for
-        #     # individual event cancellations
-        #     if 'recurringEventId' in item:
-        #         item['id'] = item['recurringEventId'] + '_' + \
-        #         datetime.datetime.strftime(
-        #             dateutil.parser.parse(item['start']['dateTime'])
-        #             .astimezone(pytz.timezone('UTC')),
-        #             '%Y%m%dT%H%M%SZ'
-        #         )
-        #     ids_to_delete.append(item.get('id', ''))
-
-        # Dedupes the list of IDs for deletion
-        ids_to_delete = list(set(ids_to_delete))
-
-        # Deletes the events you've (declined or) cancelled from the destination calendar
-        for item in ids_to_delete:
-            request_backoff(service, cal_id, 'delete', item)
-
-    return events_list
-
-
 def import_events(service, events_list, dest_cal_id):
     """ Imports list of events to the destination calendar. Uses the originating calendar to work
-     around sequence number errors if they occur. """
-
+        around sequence number errors if they occur. """
     for event in events_list:
         # Removes the immutable IDs from the event. I think it's the standard iCalUID,
         # which remains on the events, that means this script can run every hour without
         # causing duplicate events.
         event.pop('id', None)
         event.pop('recurringEventId', None)
-
         request_backoff(service, dest_cal_id, 'import', event)
 
-
 def divide_all_events(all_events):
-    """ Divides the list of all events take back out of the personal calendar into separate
-     personal and work lists. """
-
+    """ Divides the list of all events taken back out of the personal calendar into separate
+        personal and work lists. """
     work_events = personal_events = []
     for event in all_events:
-        # declined = decline_check(event, PERSONAL_EMAIL)
-
         # Strips off attendees so that you don't accidentally re-invite all your client
         # contacts to meetings that have been cancelled, or execute some similarly
         # career-limiting mistake.
         event.pop('attendees', None)
-
         # Fills the two lists based on the presence of the description field work tag.
         if 'description' in event:
             if '## sync\'d from work ##' in event['description']:
-                # # Sets the right organiser for the calendar the event is going into.
-                # event['organizer'] = {
-                #     'displayName': 'Kevin - Work only',
-                #     'email': WORK_PERSONAL_CAL_ID,
-                #     'self': True
-                # }
-                # # This next line makes sure that access to events in the work secondary
-                # # calendar (i.e. in the personal account) is restricted, with
-                # # 'private' visibility.
-                # event['visibility']	= 'private'
                 work_events.append(event)
             else:
-                # Sets the right organiser for the calendar the event is going into.
-                # event['organizer'] = {
-                #     'displayName': 'Kevin - Personal only',
-                #     'email': PERSONAL_PERSONAL_CAL_ID,
-                #     'self': True
-                # }
-                # # Converts declined attendence into having cancelled your own appointment.
-                # if declined is True:
-                #     event['status'] = 'cancelled'
                 personal_events.append(event)
         else:
             # I don't know if this happens but if there was no description at all, it couldn't
             # be a work event.
-            # event['organizer'] = {
-            #     'displayName': 'Kevin - Personal only',
-            #     'email': PERSONAL_PERSONAL_CAL_ID,
-            #     'self': True
-            # }
-            # if declined is True:
-            #     event['status'] = 'cancelled'
             personal_events.append(event)
-
     for event in work_events:
         # Sets the right organiser for the calendar the event is going into.
         event['organizer'] = {
@@ -294,7 +221,6 @@ def divide_all_events(all_events):
         # calendar (i.e. in the personal account) is restricted, with
         # 'private' visibility.
         event['visibility']	= 'private'
-    
     for event in personal_events:
         declined = decline_check(event, PERSONAL_EMAIL)
         # Sets the right organiser for the calendar the event is going into.
@@ -306,7 +232,6 @@ def divide_all_events(all_events):
         # Converts declined attendence into having cancelled your own appointment.
         if declined is True:
             event['status'] = 'cancelled'
-        
     return {'work_events': work_events, 'personal_events': personal_events}
 
 def main():
@@ -317,35 +242,35 @@ def main():
     service = discovery.build('calendar', 'v3', http=http)
 
     # Fills a biz_events list with the events from the business calendar
-    biz_events = get_events_list(service, WORK_EMAIL)
+    biz_events = get_events_list(service, WORK_EMAIL, WORK_SYNC_TOKEN)
 
     # Fills a biz_import list with the work events modified for import to personal account
-    all_biz_import = prep_import_from_work(biz_events)
+    biz_import = prep_import_from_work(biz_events)
 
     # Deletes work (declines and) cancellations from the primary personal calendar
-    biz_import = del_cancels(service, all_biz_import, PERSONAL_EMAIL)
+    # biz_import = del_cancels(service, all_biz_import, PERSONAL_EMAIL)
 
     # Imports the remaining work events into the primary personal calendar
     import_events(service, biz_import, PERSONAL_EMAIL)
 
     # Fills an all_events list with a complete list of eveything taken back from the
     # primary personal calendar
-    all_events = get_events_list(service, PERSONAL_EMAIL)
+    all_events = get_events_list(service, PERSONAL_EMAIL, PERSONAL_SYNC_TOKEN)
 
     # Fills work_ and personal_events lists with the personal account versions of events
     divided_lists = divide_all_events(all_events)
 
     # Deletes cancellations from the personal secondary calendar in the personal account
-    pp_import = del_cancels(
-        service, divided_lists.get('personal_events', None), PERSONAL_PERSONAL_CAL_ID
-    )
+    # pp_import = del_cancels(
+    #     service, divided_lists.get('personal_events', None), PERSONAL_PERSONAL_CAL_ID
+    # )
     # Imports the personal events into the personal secondary calendar in the personal account
-    import_events(service, pp_import, PERSONAL_PERSONAL_CAL_ID)
+    import_events(service, divided_lists.get('personal_events', None), PERSONAL_PERSONAL_CAL_ID)
 
     # Deletes cancellations from the work secondary calendar in the personal account
-    pw_import = del_cancels(service, divided_lists.get('work_events', None), WORK_PERSONAL_CAL_ID)
+    # pw_import = del_cancels(service, divided_lists.get('work_events', None), WORK_PERSONAL_CAL_ID)
     # Imports the work events into the work secondary calendar in the personal account
-    import_events(service, pw_import, WORK_PERSONAL_CAL_ID)
+    import_events(service, divided_lists.get('work_events', None), WORK_PERSONAL_CAL_ID)
 
 if __name__ == '__main__':
     main()
